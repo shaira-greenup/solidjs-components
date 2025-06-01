6898b1ea90a4575ec4649ca8c1530ef12ecb89ed
Shaira Greenup <shaira.greenup@gmail.com>
2025-06-01 12:46:01 +1000

    bump

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,17 +8,14 @@ export default function Nav() {
         {[
           { href: "/", label: "Home" },
           { href: "/about", label: "About" },
-          { href: "/resizer", label: "Resizer" }
-        ].map(item => (
+          { href: "/splitter", label: "Splitter" },
+        ].map((item) => (
           <NavItem href={item.href}>{item.label}</NavItem>
         ))}
       </ul>
     </nav>
   );
 }
-
-
-
 
 interface NavItemProps {
   href: string;
@@ -28,7 +25,9 @@ interface NavItemProps {
 function NavItem(props: NavItemProps) {
   const location = useLocation();
   const active = (path: string) =>
-    path == location.pathname ? "border-sky-600" : "border-transparent hover:border-sky-600";
+    path == location.pathname
+      ? "border-sky-600"
+      : "border-transparent hover:border-sky-600";
 
   return (
     <li class={`border-b-2 ${active(props.href)} mx-1.5 sm:mx-6`}>
